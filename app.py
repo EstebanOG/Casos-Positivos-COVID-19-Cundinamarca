1d15c102b8710fad52bab533ae53b4394695e834
EstebanOG <juanesteban199911@gmail.com>
2022-06-13 23:51:06 -0500

    Update html and clean dataset

--- app.py
+++ app.py
@@ -5,17 +5,17 @@ from sodapy import Socrata
 # Connection with www.datos.gov.co
 
 client = Socrata('www.datos.gov.co', None)
-result = client.get('rik9-88u9',limit=100)
+result = client.get('rik9-88u9',limit=300)
 
 result_df = pd.DataFrame.from_records(result)
-result_df
+result_df.set_index('id_de_caso', inplace = True)
 
+result_df.drop(columns= ["fecha_reporte_web","ubicacion","departamento", "departamento_nom", "fecha_de_notificaci_n", "unidad_medida","fecha_inicio_sintomas","fecha_recuperado","per_etn_","fecha_muerte","pais_viajo_1_cod","pais_viajo_1_nom", "nom_grupo_"], inplace = True)
 app = Flask(__name__)
 
 
 @app.route("/")
-def hello_world():
-    #return render_template('index.html', tables=[result_df.to_html(classes='data')])
+def main():
     return render_template('index.html',column_names=result_df.columns.values, row_data=list(result_df.values.tolist()), zip=zip)
 
 
