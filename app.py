675334c99587b712ed6ffbfda136b893ef1c6877
EstebanOG <juanesteban199911@gmail.com>
2022-06-14 10:11:38 -0500

    Update filters

--- app.py
+++ app.py
@@ -24,6 +24,11 @@ result_df.columns=['Código Municipio',
                     'Fecha de Diagnostico',
                     'Tipo de Recuperación']
 
+lista_municipios = []
+for i in result_df['Municipio']:
+  if i not in lista_municipios:
+    lista_municipios.append(i)
+
 app = Flask(__name__)
 
 
@@ -32,22 +37,49 @@ def main():
     if request.method == 'POST':
         sexo = request.form.get("Sexo")
         estado = request.form.get("Estado")
+        edad = request.form.get("Edad")
+        municipio = request.form.get("Municipio")
         results_df = result_df
+        # SEXO
         if sexo == 'Mujer':
             results_df = results_df[results_df["Sexo"] == "F"]
 
         if sexo == 'Hombre':
             results_df = results_df[results_df["Sexo"] == "M"]
         
+        # ESTADO
         if estado == 'Fallecido':
             results_df = results_df[results_df["Estado"] == "Fallecido"] 
 
         if estado == 'Leve':
             results_df = results_df[results_df["Estado"] == "Leve"]
-            
-        return render_template('index.html',column_names=result_df.columns.values, row_data=list(results_df.values.tolist()), zip=zip)
+
+        # EDAD
+        if edad == 'PrimeraInfancia':
+            results_df = results_df[pd.to_numeric(results_df['Edad']) <= 5]
+        
+        if edad == 'Infancia':
+            results_df = results_df[(pd.to_numeric(results_df['Edad']) >= 6) & (pd.to_numeric(results_df['Edad']) <= 11)]
+
+        if edad == 'Adolescencia':
+            results_df = results_df[(pd.to_numeric(results_df['Edad']) >= 12) & (pd.to_numeric(results_df['Edad']) <= 18)]
+        
+        if edad == 'Juventud':
+            results_df = results_df[(pd.to_numeric(results_df['Edad']) >= 19) & (pd.to_numeric(results_df['Edad']) <= 26)]
+        
+        if edad == 'Adultez':
+            results_df = results_df[(pd.to_numeric(results_df['Edad']) >= 27) & (pd.to_numeric(results_df['Edad']) <= 59)]
+
+        if edad == 'PersonaMayor':
+            results_df = results_df[ pd.to_numeric(results_df['Edad']) >= 60 ]
+        
+        # MUNICIPIO
+        if municipio in lista_municipios:
+            results_df = results_df[results_df["Municipio"] == municipio]
+
+        return render_template('index.html',column_names=result_df.columns.values, row_data=list(results_df.values.tolist()), zip=zip, municipios = lista_municipios)
     else:
-        return render_template('index.html',column_names=result_df.columns.values, row_data=list(result_df.values.tolist()), zip=zip)
+        return render_template('index.html',column_names=result_df.columns.values, row_data=list(result_df.values.tolist()), zip=zip, municipios = lista_municipios)
 
 
 if __name__ == '__main__':
